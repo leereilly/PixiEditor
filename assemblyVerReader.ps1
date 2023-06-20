921bb99938f98ed88839d92edf458070fd2c834e
Software Provider <118369443+software-provider@users.noreply.github.com>
2023-06-21 07:26:52 +0900

    Update assemblyVerReader.ps1

--- assemblyVerReader.ps1
+++ assemblyVerReader.ps1
@@ -3,4 +3,4 @@ $assemblyInfoPath = "src\PixiEditor\Properties\AssemblyInfo.cs"
 $contents = [System.IO.File]::ReadAllText($assemblyInfoPath)
 
 $versionString = [RegEx]::Match($contents,"(?:\d+\.\d+\.\d+\.\d+)")
-Write-Host "##vso[task.setvariable variable=TagVersion;]$versionString"
+Write-Host "##vso[task.setvariable variable=TagVersion;]$versionString" 
